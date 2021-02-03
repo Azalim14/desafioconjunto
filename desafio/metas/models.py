a87352fc664f5b5d087c101dee84076d2e1a67cd
raphaelrss <raphaelrss.profissional@gmail.com>
2021-02-03 14:17:55 -0300

    implementa models comentario e setor

--- desafio/metas/models.py
+++ desafio/metas/models.py
@@ -8,6 +8,13 @@ STATUS = (
     ('done', 'Done')
 )
 
+class Setor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Meta(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -15,6 +22,13 @@ class Meta(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    fk_setor = models.ForeignKey(Setor, on_delete = models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Comentario(models.Model):
+    fk_meta = models.ForeignKey(Meta, on_delete = models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
